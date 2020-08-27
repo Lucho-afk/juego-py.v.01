a8bfacdd5799abd528529e61bd5479126ed1d9ac
Luciano <lucianoleone2011@hotmail.es>
2020-08-27 18:15:50 -0300

    estoy toquteando el branch

--- animaciones.py
+++ animaciones.py
@@ -9,11 +9,14 @@ tam=(500,500)
 pantalla=pygame.display.set_mode(tam)
 clock=pygame.time.Clock()
 
+
+
 copos_sangrientos=[]
 for i in range(60):
     #importar libreria random, randint lo que hace es enviar al azar un numero entre los rangos
     x=random.randint(0,800)
     y=random.randint(0,500)
+    #creo la lista de copitos, [x,y] es una coordenada dentro del cuaderno
     copos_sangrientos.append ([x,y])
 
 while True:
@@ -25,11 +28,13 @@ while True:
     pantalla.fill(color)
     for i in copos_sangrientos:
     #importar libreria random, randint lo que hace es enviar al azar un numero entre los rangos
-
         pygame.draw.circle(pantalla, red,i,2)
         i[1]+=1
         if i[1]>500:
             i[1]=0
+
+    mouse_pos=pygame.mouse.get_pos()
+
 
     pygame.display.update()
     clock.tick(30)
