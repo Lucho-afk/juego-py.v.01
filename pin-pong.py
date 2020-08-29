4c5c5cde476d2ce52456249422a56471ebc79398
Luciano <lucianoleone2011@hotmail.es>
2020-08-28 23:00:34 -0300

    termine el pin pong

--- pin-pong.py
+++ pin-pong.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 pygame.init()
 
 #colores
@@ -25,8 +25,8 @@ velocidad_x_d=0
 #pelota
 cordenada_x_p=400
 cordenada_y_p=300
-velocidad_x_p=1
-velcoidad_y_p=3
+velocidad_x_p=random.choice([-3,3])
+velocidad_y_p=random.choice([-3,3])
 #juego
 while not game_over:
     for event in pygame.event.get():
@@ -46,20 +46,27 @@ while not game_over:
         #PALETA DE LA DERECHA
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_UP:
-                velocidad_y_d=-6
-            if event.key==pygame.K_DOWN:
-                velocidad_y_d=6
+                if cordenada_y_i>0:
+
+                    velocidad_y_d=-6
+            if event.key==pygame.K_DOWN and cordenada_y_i<510:
+
+                    velocidad_y_d=6
         if event.type==pygame.KEYUP:
             if event.key==pygame.K_UP:
                 velocidad_y_d=0
             if event.key==pygame.K_DOWN:
                 velocidad_y_d=0
-
-    cordenada_y_p+=velcoidad_y_p
+    #esto es pa que no se vaya por arriba y vuelva al centro por los costados
+    cordenada_y_p+=velocidad_y_p
+    cordenada_x_p+=velocidad_x_p
     if cordenada_y_p>590 or cordenada_y_p<0:
-        velcoidad_y_p*=-1
-
-
+        velocidad_y_p*=-1
+    if cordenada_x_p>830 or cordenada_x_p<-30:
+        cordenada_x_p=400
+        cordenada_y_p=300
+        velocidad_x_p*=random.choice([1,-1])
+        velocidad_y_p*=random.choice([1,-1])
 
     pantalla.fill(negro)
     #zona de dibujo
@@ -67,9 +74,13 @@ while not game_over:
     cordenada_y_d+=velocidad_y_d
 
 
-    pygame.draw.circle(pantalla, blanco,(cordenada_x_p,cordenada_y_p),10)
-    pygame.draw.rect(pantalla, blanco, (cordenada_x_i,cordenada_y_i,15,90))
-    pygame.draw.rect(pantalla, blanco, (cordenada_x_d,cordenada_y_d,15,90))
+    pelota=pygame.draw.circle(pantalla, blanco,(cordenada_x_p,cordenada_y_p),10)
+    jugador1=pygame.draw.rect(pantalla, blanco, (cordenada_x_i,cordenada_y_i,15,90))
+    jugador2=pygame.draw.rect(pantalla, blanco, (cordenada_x_d,cordenada_y_d,15,90))
+
+    if pelota.colliderect(jugador1) or pelota.colliderect(jugador2):
+        velocidad_x_p*=-1
+        velocidad_y_p*=random.choice([1,-1])
 
     pygame.display.flip()
     clock.tick(60)
